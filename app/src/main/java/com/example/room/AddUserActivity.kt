21fe9a0566c8eabfd181a7c61e15b02fe081cf0c
unknown <edwinngera@gmail.com>
2022-05-20 07:41:33 +0300

    first commit

---
+++ app/src/main/java/com/example/room/AddUserActivity.kt
@@ -0,0 +1,11 @@
+package com.example.room
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class AddUserActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_user)
+    }
+}
---
+++ app/src/main/java/com/example/room/UserListAdapter.java
@@ -0,0 +1,4 @@
+package com.example.room;
+
+public class UserListAdapter {
+}
